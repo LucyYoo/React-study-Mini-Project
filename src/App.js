fde9230de70530cb7acc8d86930e50136e2ce850
유지영 <yj0zero@gmail.com>
2022-10-29 20:46:09 +0900

    feat: 빈 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
+import EmptyPate from "./component/page/EmptyPage";
 import MainPage from "./component/page/MainPage";
 import PostViewPage from "./component/page/PostViewPage";
 import PostWritePage from "./component/page/PostWritePage";
@@ -9,16 +10,17 @@ const MainTitleText = styled.p`
   font-size: 24px;
   font-weight: bold;
   text-align: centder;
-`
+`;
 
 function App() {
   return (
     <BrowserRouter>
       <MainTitleText>제로의 미니 블로그</MainTitleText>
       <Routes>
-          <Route index element={<MainPage />} />
-          <Route path="post-write" element={<PostWritePage />} />
-          <Route path="post/:postId" element={<PostViewPage />} />
+        <Route index element={<MainPage />} />
+        <Route path="post-write" element={<PostWritePage />} />
+        <Route path="post/:postId" element={<PostViewPage />} />
+        <Route path="*" element={<EmptyPate />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/component/page/EmptyPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function EmptyPate() {
+  return (
+    <>
+        <p>
+            존재하지 않는 페이지입니다.
+        </p>
+        <Link to='/'>메인 페이지로</Link>
+    </>
+    
+}
