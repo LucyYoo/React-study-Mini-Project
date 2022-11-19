6fccb49add6c7c9c61677820e33031a3593e3ad8
유지영 <yj0zero@gmail.com>
2022-11-19 21:02:34 +0900

    feat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import EmptyPate from "./component/page/EmptyPage";
 import HeaderPage from "./component/page/HeaderPage";
 import LoginPage from "./component/page/LoginPage";
@@ -11,10 +10,9 @@ import PostWritePage from "./component/page/PostWritePage";
 function App() {
   return (
     <BrowserRouter>
-      <HeaderPage />
       <Routes>
         <Route index element={<LoginPage />} />
-        <Route path="main" element={<MainPage />} />
+        <Route path="main" element={<MainPage l />} />
         <Route path="post-write" element={<PostWritePage />} />
         <Route path="post/:postId" element={<PostViewPage />} />
         <Route path="*" element={<EmptyPate />} />
